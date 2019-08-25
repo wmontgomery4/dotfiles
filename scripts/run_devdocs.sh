82d3e851d4829732d479d3a49b11c5d81df9177d
John <indianajohn@github>
2019-08-24 19:07:08 -0700

    making this work better.

--- scripts/run_devdocs.sh
+++ scripts/run_devdocs.sh
@@ -2,7 +2,10 @@ mkdir -p $HOME/dev
 cd $HOME/dev/
 if [ ! -d $PWD/devdocs ]; then
   git clone https://github.com/freeCodeCamp/devdocs.git && cd devdocs
+  gem update --system
+  sudo gem uninstall bundler
   sudo gem install bundler
+  bundler update --bundler
   bundle install
   bundle exec thor docs:download --default
   bundle exec thor docs:download c cpp
